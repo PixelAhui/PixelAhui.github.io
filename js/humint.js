ab330be672159bf7cf887e7a60d01588c9546fe6
user.email <pixels.games.company@gmail.com>
2026-02-15 23:55:22 -0600

    Reto HUMINT protect1

--- js/humint.js
+++ js/humint.js
@@ -1,17 +1,107 @@
-function validar() {
-    const _0x4b21 = ['value', 'error-msg', 'getElementById', 'donpepe_azul', '190223', '¡ACCESO\x20CONCEDIDO!\x20FLAG{HUMINT_MASTER_RECOVERED}', 'https://pixelahui.com', 'display', 'block', 'style', 'toLowerCase'];
-    const _0x2f1a = function(_0x4b21d2, _0x2f1a34) {
-        return _0x4b21[_0x4b21d2 = _0x4b21d2 - 0x0];
-    };
-    
-    const u = document[_0x2f1a('0x2')]('user')[_0x2f1a('0x0')];
-    const p = document[_0x2f1a('0x2')]('pass')[_0x2f1a('0x0')];
-    const error = document[_0x2f1a('0x2')](_0x2f1a('0x1'));
+const hola = "hola mundo";
 
-    if (u[_0x2f1a('0xa')]() === _0x2f1a('0x3') && p === _0x2f1a('0x4')) {
-        alert(_0x2f1a('0x5'));
-        window.location.href = _0x2f1a('0x6');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function validar() {const _0x1a2b = ['value', 'error-msg', 'getElementById', 'dalmaciorangelgarza1502', 'Dalmacio01101980', '¡ACCESO\x20CONCEDIDO!\x20FLAG{HUMINT_MASTER_RECOVERED}', 'https://pixelahui.com', 'display', 'block', 'style', 'toLowerCase'];const _0x55cf = function(_0x1a2b3c, _0x55cf12) {        return _0x1a2b[_0x1a2b3c = _0x1a2b3c - 0x0];};const userIn = document[_0x55cf('0x2')]('user')[_0x55cf('0x0')];const passIn = document[_0x55cf('0x2')]('pass')[_0x55cf('0x0')];const errorDiv = document[_0x55cf('0x2')](_0x55cf('0x1'));
+    if (userIn[_0x55cf('0xa')]() === _0x55cf('0x3') && passIn === _0x55cf('0x4')) {
+        alert(_0x55cf('0x5'));
+        window.location.href = _0x55cf('0x6');
     } else {
-        error[_0x2f1a('0x9')][_0x2f1a('0x7')] = _0x2f1a('0x8');
+        errorDiv[_0x55cf('0x9')][_0x55cf('0x7')] = _0x55cf('0x8');
     }
 }
