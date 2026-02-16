6c1ea79c6e6b0721c4678fb905a442b44d023068
user.email <pixels.games.company@gmail.com>
2026-02-15 23:47:08 -0600

    Reto HUMINT protect

--- js/humint.js
+++ js/humint.js
@@ -1,14 +1,17 @@
 function validar() {
-        const u = document.getElementById('user').value;
-        const p = document.getElementById('pass').value;
-        const error = document.getElementById('error-msg');
+    const _0x4b21 = ['value', 'error-msg', 'getElementById', 'donpepe_azul', '190223', '¡ACCESO\x20CONCEDIDO!\x20FLAG{HUMINT_MASTER_RECOVERED}', 'https://pixelahui.com', 'display', 'block', 'style', 'toLowerCase'];
+    const _0x2f1a = function(_0x4b21d2, _0x2f1a34) {
+        return _0x4b21[_0x4b21d2 = _0x4b21d2 - 0x0];
+    };
+    
+    const u = document[_0x2f1a('0x2')]('user')[_0x2f1a('0x0')];
+    const p = document[_0x2f1a('0x2')]('pass')[_0x2f1a('0x0')];
+    const error = document[_0x2f1a('0x2')](_0x2f1a('0x1'));
 
-        // AQUÍ DEFINES TU FLAG O RESPUESTA CORRECTA
-        // Basado en lo que investigaron de Don Pepe
-        if(u.toLowerCase() === "donpepe_azul" && p === "190223") { 
-            alert("¡ACCESO CONCEDIDO! FLAG{HUMINT_MASTER_RECOVERED}");
-            window.location.href = "https://pixelahui.com"; 
-        } else {
-            error.style.display = "block";
-        }
+    if (u[_0x2f1a('0xa')]() === _0x2f1a('0x3') && p === _0x2f1a('0x4')) {
+        alert(_0x2f1a('0x5'));
+        window.location.href = _0x2f1a('0x6');
+    } else {
+        error[_0x2f1a('0x9')][_0x2f1a('0x7')] = _0x2f1a('0x8');
     }
+}
